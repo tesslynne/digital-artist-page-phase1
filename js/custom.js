2142303ac723fd32e350ad02555e26d22bd2112e
Tess DiNapoli <Tess DiNapoli>
2016-02-09 18:44:45 -0800

    working on modal video problem

--- js/custom.js
+++ js/custom.js
@@ -8,8 +8,8 @@ The amount of time to delay between automatically cycling an item.
 If false, carousel will not automatically cycle.
 */
 
-$('.carousel').carousel() {
-	interval: 500;
+$(".carousel").carousel( {
+	interval: 500
 });
 
 
@@ -18,3 +18,42 @@ $('.carousel').carousel() {
 Note default behavior is
 Pauses the cycling of the carousel on mouseenter and resumes the cycling of the carousel on mouseleave.
 */
+
+//script for VIDEO CONTENT in modals//
+
+// $(document).ready(function(){
+//     /* Get iframe src attribute value i.e. YouTube video url
+//     and store it in a variable */
+//     var url = $("civilwar-trailer").attr("src")
+    
+//     /* Assign empty url value to the iframe src attribute when
+//     modal hides, which stops the video playing */
+//     $("civilwar").on("hide.bs.modal", function(){
+//         $("civilwar-trailer").attr("src", "")
+//     });
+    
+//     /* Assign the initially stored url back to the iframe src
+//     attribute when modal is displayed again */
+//     $("civilwar").on("show.bs.modal", function(){
+//         $("civilwar-trailer").attr("src", url)
+//     });
+// });
+
+// $(document).ready(function(){
+//     $('.modal fade').each(function(){
+//             var src = $(this).find('iframe').attr('src');
+
+//         $(this).on('click', function(){
+
+//             $(this).find('iframe').attr('src', '');
+//             $(this).find('iframe').attr('src', src);
+
+//         });
+//     });
+// });
+
+$('#civilwar').on('hidden.bs.modal', function () {
+	console.log("modalclose")
+	$('iframe').trigger('pause');
+});
+console.log("hey")
